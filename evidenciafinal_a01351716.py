4a5fbcf3d3596f32b7882dcfcc5491b107302784
renatachavezm22 <124286680+renatachavezm22@users.noreply.github.com>
2023-06-05 19:05:33 -0600

    Update evidenciafinal_a01351716.py

--- evidenciafinal_a01351716.py
+++ evidenciafinal_a01351716.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.title ('Police Incident Reports from 2018 and 2020 in San Francisco')
-st.header('Police Incident Reports from 2018 and 2020 in San Francisco')
+st.header('Introduction')
 
 df = pd.read_csv("https://drive.google.com/file/d/1ioUp8979nNLh9h-CYVyJcRgAhxBhCLIm/view")
 
@@ -11,3 +11,12 @@ st.markdown('The data shown below belongs to incident reports in the city of San
 mapa=pd.DataFrame()
 mapa=mapa.dropna()
 st.map(mapa.astype(int))
+
+categories = st.multiselect('Select Categories', df['category'].unique())
+filtered_df = df[df['category'].isin(categories)]
+
+st.subheader('Filtered Incident Reports')
+st.dataframe(filtered_df)
+
+st.subheader('Summary Statistics')
+st.write(filtered_df.describe())
