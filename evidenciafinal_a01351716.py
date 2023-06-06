38fc1a8d3ad48b126a76b260f6a1108f85b04a73
renatachavezm22 <124286680+renatachavezm22@users.noreply.github.com>
2023-06-05 19:08:36 -0600

    Update evidenciafinal_a01351716.py

--- evidenciafinal_a01351716.py
+++ evidenciafinal_a01351716.py
@@ -6,12 +6,6 @@ st.header('Introduction')
 
 df = pd.read_csv("https://drive.google.com/file/d/1ioUp8979nNLh9h-CYVyJcRgAhxBhCLIm/view")
 
-df_sf = df[df['neighborhood'].str.contains('San Francisco', case=False)]
-
-st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location, and resolution.')
-
-st.dataframe(df_sf)
-
 mapa=pd.DataFrame()
 mapa=mapa.dropna()
 st.map(mapa.astype(int))
