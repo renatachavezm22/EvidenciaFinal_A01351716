2b3d2abfe1c3119c7035b675815839a179a3f240
renatachavezm22 <124286680+renatachavezm22@users.noreply.github.com>
2023-06-05 19:10:23 -0600

    Update evidenciafinal_a01351716.py

--- evidenciafinal_a01351716.py
+++ evidenciafinal_a01351716.py
@@ -1,13 +1,17 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
-st.title ('Police Incident Reports from 2018 and 2020 in San Francisco')
-st.header('Introduction')
+st.header(':blue[Police Incident Reports from 2018 to 2020 in San Francisco] :police_car: :us:')
+df=pd.read_csv("https://drive.google.com/file/d/11oLcKiW8SgCOp3tGiQCYuRG7pLL_J-Zf/view?usp=share_link/Police_Department_Incident_Reports__2018_to_Present.csv")
 
-df = pd.read_csv("https://drive.google.com/file/d/1ioUp8979nNLh9h-CYVyJcRgAhxBhCLIm/view")
+st.markdown("The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.")
 
-mapa=pd.DataFrame()
+mapa=pd.DataFrame(
+        np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon']
+)
 mapa=mapa.dropna()
-st.map(mapa.astype(int))
+st.map(mapa)
 
 st.write(f'Total Records: {len(df)}')
