1cb8d0a0a9e1108f04173abd064ef6dd9a6bb9f9
renatachavezm22 <124286680+renatachavezm22@users.noreply.github.com>
2023-06-06 19:12:55 -0600

    Update evidenciafinal_a01351716.py

--- evidenciafinal_a01351716.py
+++ evidenciafinal_a01351716.py
@@ -1,25 +1,25 @@
- import streamlit as st
-import pandas as pd
+import streamlit as st
 #import numpy as np
+import pandas as pd
 #import plotly as px
-#import plotly.figure_factory as ff
-#from bokeh.plotting import figure
+#import plotly.figure_factury as ff
+#from brokeh.plotting import figure
 #import matplotlib.pyplot as plt
 
-st.title ('Police Incident Reports from 2018 and 2020 in San Francisco')
-
-df = pd.read_csv("Police_Department_Incident_Reports__2018_to_Present.csv")
+st.title('Police Incidents Reports from 2018 to 2020 in San Francisco')
+df = pd.read_csv('Police_Department_Incident_Reports__2018_to_Present.csv')
 st.dataframe(df)
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
 
-mapa=pd.DataFrame()
-mapa['Date']=df['Incident Date']
-mapa['Day']=df['Incident Day of the Week']
-mapa['Police District']=df['Police District']
-mapa['Neighborhood']=df['Analysis Neoghborhood']
-mapa['Incident Category']=df['Incident Category']
+mapa = pd.DataFrame()
+mapa['Date']= df['Incident Date']
+mapa['Day']= df['Incident Day of Week']
+mapa['Police District']= df['Police District']
+mapa['Neighborhood']= df['Analysis Neighborhood']
+mapa['Incident Category']= df['Incident Subcategory']
 mapa['Resolution'] = df['Resolution']
 mapa['lat'] = df['Latitude']
 mapa['lon'] = df['Longitude']
+mapa = pd.DataFrame(df['Latitude'],df['Longitude'])
 mapa = mapa.dropna()
 st.map(mapa.astype(int))
