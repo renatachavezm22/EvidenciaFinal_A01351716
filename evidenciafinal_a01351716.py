a6bbe10bd601e96aa1b4928e86c259a44a2cbad8
renatachavezm22 <124286680+renatachavezm22@users.noreply.github.com>
2023-06-06 18:26:26 -0600

    Update evidenciafinal_a01351716.py

--- evidenciafinal_a01351716.py
+++ evidenciafinal_a01351716.py
@@ -20,14 +20,14 @@ st.map(mapa)
 '''
 
 mapa=pd.DataFrame()
-mapa=['Date']=df['Incident Date']
-mapa=['Day']=df['Incident Day of Week]
-mapa=['Police District']=df['Police District']
-mapa=['Neighborhood']=df['Analysis Neighborhood']
-mapa=['Incident Category']=df['Incident Category']
-mapa=['Resolution']=df['Resolution']
-mapa=['lat']=df['Latitude']
-mapa=['lon']=df['Longitude']
+mapa['Date']=df['Incident Date']
+mapa['Day']=df['Incident Day of Week]
+mapa['Police District']=df['Police District']
+mapa['Neighborhood']=df['Analysis Neighborhood']
+mapa['Incident Category']=df['Incident Category']
+mapa['Resolution']=df['Resolution']
+mapa['lat']=df['Latitude']
+mapa['lon']=df['Longitude']
 mapa=mapa.dropna()
    
 
