24238dc2babedaf0633b8f1112fe2ec156eb29b4
renatachavezm22 <124286680+renatachavezm22@users.noreply.github.com>
2023-06-06 18:26:57 -0600

    Update evidenciafinal_a01351716.py

--- evidenciafinal_a01351716.py
+++ evidenciafinal_a01351716.py
@@ -21,7 +21,7 @@ st.map(mapa)
 
 mapa=pd.DataFrame()
 mapa['Date']=df['Incident Date']
-mapa['Day']=df['Incident Day of Week]
+mapa['Day']=df['Incident Day of Week']
 mapa['Police District']=df['Police District']
 mapa['Neighborhood']=df['Analysis Neighborhood']
 mapa['Incident Category']=df['Incident Category']
